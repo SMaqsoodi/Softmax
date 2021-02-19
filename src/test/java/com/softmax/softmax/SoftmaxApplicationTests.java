3c2846fd8d3cd67e4f5a017056bc2e4dc3e1bbbe
SMaqsoodi <saeedreza.maqsoodi@gmail.com>
2021-02-19 14:50:09 -0800

    added maven project files

---
+++ src/test/java/com/softmax/softmax/SoftmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmax.softmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
